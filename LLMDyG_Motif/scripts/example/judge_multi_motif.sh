4fbdcc175f39b8cbcaafa1b611e2bc0384659a09
郝冰 <78463725+irisorchid107@users.noreply.github.com>
2025-11-14 13:53:32 +0800

    Delete LLMDyG_Motif directory

--- LLMDyG_Motif/scripts/example/judge_multi_motif.sh
+++
@@ -1,8 +0,0 @@
-python /home/ma-user/work/LLMDyG_Motif/scripts/example/run_one_task.py --task judge_multi_motif --T 15 --N 20 --p 0.3 --model pangu_auto --motif 1 --multi_motif 1 --k 0 --max_tokens 20480 --num_seed 100 --change 2 -t run
-python /home/ma-user/work/LLMDyG_Motif/scripts/example/run_one_task.py --task judge_multi_motif --T 15 --N 20 --p 0.3 --model pangu_auto --motif 1 --multi_motif 1 --k 0 --max_tokens 20480 --num_seed 100 --change 2 -t eval
-
-python /home/ma-user/work/LLMDyG_Motif/scripts/example/run_one_task.py --task multi_motif_first_time --T 15 --N 20 --p 0.3 --model pangu_auto --motif 1 --multi_motif 1 --k 0 --max_tokens 20480 --num_seed 100 --change 2 -t run
-python /home/ma-user/work/LLMDyG_Motif/scripts/example/run_one_task.py --task multi_motif_first_time --T 15 --N 20 --p 0.3 --model pangu_auto --motif 1 --multi_motif 1 --k 0 --max_tokens 20480 --num_seed 100 --change 2 -t eval
-
-python /home/ma-user/work/LLMDyG_Motif/scripts/example/run_one_task.py --task multi_motif_count --T 15 --N 20 --p 0.3 --model pangu_auto --motif 1 --multi_motif 1 --k 0 --max_tokens 20480 --num_seed 100 --change 2 -t run
-python /home/ma-user/work/LLMDyG_Motif/scripts/example/run_one_task.py --task multi_motif_count --T 15 --N 20 --p 0.3 --model pangu_auto --motif 1 --multi_motif 1 --k 0 --max_tokens 20480 --num_seed 100 --change 2 -t eval
